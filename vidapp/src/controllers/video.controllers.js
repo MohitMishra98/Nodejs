22af2ee8db57eda425d2f3c1dd5d5a208b6aa9b3
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2025-11-04 08:10:32 +0000

    added the get all video controller

--- vidapp/src/controllers/video.controllers.js
+++ vidapp/src/controllers/video.controllers.js
@@ -9,11 +9,64 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
+
+  const pageNo = parseInt(page);
+  const limitNo = parseInt(limit);
+
+  const skipby = (pageNo - 1) * limitNo;
+
+  // dynamic pipeline constructor
+  const pipeline = {};
+
+  if (userId) {
+    pipeline.owner = new mongoose.Types.ObjectId(String(userId));
+  }
+  if (query) {
+    pipeline.$or = [
+      { title: { $regex: query, $options: "i" } },
+      { description: { $regex: query, $options: "i" } },
+    ];
+  }
+
+  // this basically creates a pipeline like this
+  // (object.name creates a new field in that object)
+  // (pipeline.owner will create a new key value pair owner as key object id as value)
+  // {
+  //   $match: {
+  //     owner: new mongoose.Types.ObjectId(String(userId)),
+  //     $or = [
+  //       { title: { $regex: query, $options: "i" } },
+  //       { description: { $regex: query, $options: "i" } },
+  //     ]
+  //   }
+  // }
+
+  const data = await Video.aggregate([
+    {
+      $match: pipeline,
+    },
+    {
+      $sort: {
+        [sortBy || "createdAt"]: sortType === "desc" ? -1 : 1,
+      },
+    },
+    {
+      $skip: skipby || 0,
+    },
+    {
+      $limit: limitNo || 10,
+    },
+  ]);
+
+  if (data.length === 0) {
+    throw new ApiError(404, "No videos with given parameters found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, data, "video fetched successfully"));
 });
 
-// this should be a secured route
-// TODO: make sure to add it in the secured route section
-// also make sure that the multer is setup properly in the routes as middlewares
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
   // TODO: get video, upload to cloudinary, create video
@@ -72,20 +125,121 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
+  if (!videoId) {
+    throw new ApiError(400, "video id not provided");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "video not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "video fetched successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
+
+  if (!videoId) {
+    throw new ApiError(400, "video is is not provided");
+  }
+
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    throw new ApiError(400, "title or description is not provided");
+  }
+
+  const thumbnailPath = req.files?.thumbnail[0].path;
+  if (!thumbnailPath) {
+    throw new ApiError(404, "thumbnail path is not found");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "video not found");
+  }
+
+  console.log(video.owner, req.user._id);
+
+  if (!video.owner.equals(req.user._id)) {
+    throw new ApiError(401, "video does not belongs to user");
+  }
+
+  const thumbnailCloud = await uploadOnCloudinary(thumbnailPath);
+
+  if (!thumbnailCloud.url) {
+    throw new ApiError(400, "not uploaded to cloud");
+  }
+
+  const newVideo = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      description,
+      title,
+      thumbnail: thumbnailCloud.url,
+    },
+    { new: true }
+  );
+
+  if (!newVideo) {
+    throw new ApiError(400, "video not updated");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newVideo, "video updated successfully"));
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: delete video
+
+  if (!videoId) {
+    throw new ApiError(400, "video id is not provided");
+  }
+
+  const video = await Video.findOneAndDelete({
+    $and: [
+      { _id: videoId },
+      { owner: new mongoose.Types.ObjectId(String(req.user._id)) },
+    ],
+  });
+
+  if (!video) {
+    throw new ApiError(400, "video not found or not owned by user");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "video deleted successfully"));
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "video id not found");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video.owner.equals(req.user._id)) {
+    throw new ApiError(401, "User is not the owner of video");
+  }
+
+  video.isPublished = !video?.isPublished;
+
+  await video.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, [], "published status toggled"));
 });
 
 export {
--- vidapp/src/routes/video.routes.js
+++ vidapp/src/routes/video.routes.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
-import { publishAVideo } from "../controllers/video.controllers.js";
+import {
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+  getAllVideos,
+} from "../controllers/video.controllers.js";
 
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
+// unsecured routes
+router.route("/get-video/:videoId").get(getVideoById);
+router.route("/get-videos").get(getAllVideos)
+
+// secured routes
 router.route("/upload").post(
   verifyJWT,
   upload.fields([
@@ -20,5 +32,22 @@ router.route("/upload").post(
   ]),
   publishAVideo
 );
+
+router.route("/update-video/:videoId").patch(
+  verifyJWT,
+  upload.fields([
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+  ]),
+  updateVideo
+);
+
+router.route("/delete/:videoId").delete(verifyJWT, deleteVideo);
+
+router
+  .route("/toggle-published/:videoId")
+  .patch(verifyJWT, togglePublishStatus);
 
 export default router;
