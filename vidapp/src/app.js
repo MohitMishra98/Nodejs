2cea49162b3f7a5d3ccb00089cb2c16d4d2a03af
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2025-11-05 07:42:04 +0000

    added the likes controller

--- vidapp/src/app.js
+++ vidapp/src/app.js
@@ -24,6 +24,7 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import likeRouter from "./routes/like.routers.js";
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter);
@@ -31,5 +32,6 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/likes", likeRouter);
 
 export { app };
---
+++ vidapp/src/routes/like.routers.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  getLikedVideos,
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+} from "../controllers/like.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+// unsecured routes
+
+// secured routes
+router.route("/toggle-video-like/:videoId").post(verifyJWT, toggleVideoLike);
+router
+  .route("/toggle-comment-like/:commentId")
+  .post(verifyJWT, toggleCommentLike);
+router.route("/toggle-tweet-like/:tweetId").post(verifyJWT, toggleTweetLike);
+router.route("/get-liked-videos").get(verifyJWT, getLikedVideos);
+
+export default router;
